65f7a679b26934a85818511912c6697b4d4c5fa4
Sonya Wong <sonyawongpro@gmail.com>
2018-08-30 17:04:11 +0800

    setting page

--- frontend/src/screens/Settings.tsx
+++ frontend/src/screens/Settings.tsx
@@ -1,23 +1,157 @@
-import * as React from 'react';
-import LinearGradient from 'react-native-linear-gradient';
-import {
-    StyleSheet, 
-    Text, 
-    View
-} from 'react-native';
+import * as React from "react";
+import RangeSlider from "react-native-range-slider";
+import ToggleSwitch from "toggle-switch-react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+
+//container design -boxess
+//notification: toggle button
+//age preference: slider;
 
 interface SettingsProps {
-    navigator: Navigator
+  navigator: Navigator;
 }
-export default class Settings extends React.Component<SettingsProps> {
+
+interface SettingsState {
+  isOnMenSwitch: boolean;
+  isOnWomenSwitch: boolean;
+  isOnMatchesSwitch: boolean;
+  isOnMessageSwitch: boolean;
+  isOnRequestSwitch: boolean;
+  isOnMessagesSwitch: boolean;
+}
+
+export default class Settings extends React.Component<
+  SettingsProps,
+  SettingsState
+> {
+  state = {
+    isOnMenSwitch: false,
+    isOnWomenSwitch: false,
+    isOnMatchesSwitch: false,
+    isOnRequestSwitch: false,
+    isOnMessagesSwitch: false
+  };
+
+  onToggle(isOn) {
+    return isOn;
+  }
+
   render() {
     return (
-      <LinearGradient colors={['#9EF8E4', '#30519B']} style={[{flex: 1}]}>
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Settings here</Text>
+      <LinearGradient colors={["#9EF8E4", "#30519B"]} style={[{ flex: 1 }]}>
+        <View style={styles.container}>
+          <View style={styles.agerow}>
+            <Text style={styles.header}>Age Range</Text>
+            <RangeSlider
+              minValue={20}
+              maxValue={60}
+              tintColor={"rgb(0,96,255)"}
+              handleBorderWidth={1}
+              handleBorderColor="rgb(0,96,255)"
+              selectedMinimum={20}
+              selectedMaximum={50}
+              style={{
+                flex: 1,
+                height: 70,
+                padding: 30,
+                backgroundColor: "rgb(232,238,241)"
+              }}
+              onChange={(data: any) => {
+                console.log(data);
+              }}
+            />
+          </View>
 
+          <View style={styles.showmerow}>
+            <Text style={styles.header}>Show Me</Text>
+            <ToggleSwitch
+              onColor="rgb(253,161,144)"
+              offColor="rgb(83,165,196)"
+              label="Men"
+              labelStyle={{
+                color: "rgb(83,165,196)",
+                fontWeight: "500",
+                fontSize: 20
+              }}
+              size="large"
+              isOn={this.state.isOnMenSwitch}
+              onToggle={isOnMenSwitch => {
+                this.setState({ isOnMenSwitch });
+                this.onToggle(isOnMenSwitch);
+              }}
+            />
+            <ToggleSwitch
+              onColor="rgb(253,161,144)"
+              offColor="rgb(83,165,196)"
+              label="Women"
+              labelStyle={{
+                color: "rgb(83,165,196)",
+                fontWeight: "500",
+                fontSize: 20
+              }}
+              size="large"
+              isOn={this.state.isOnWomenSwitch}
+              onToggle={isOnWomenSwitch => {
+                this.setState({ isOnWomenSwitch });
+                this.onToggle(isOnWomenSwitch);
+              }}
+            />
 
-      </View>
+            <View style={styles.messagerow}>
+              <Text style={styles.header}>Notification</Text>
+              <ToggleSwitch
+                //isOn={false}
+                onColor="rgb(253,161,144)"
+                offColor="rgb(83,165,196)"
+                label="Matches"
+                labelStyle={{
+                  color: "rgb(83,165,196)",
+                  fontWeight: "500",
+                  fontSize: 20
+                }}
+                size="large"
+                isOn={this.state.isOnMatchesSwitch}
+                onToggle={isOnMatchesSwitch => {
+                  this.setState({ isOnMatchesSwitch });
+                  this.onToggle(isOnMatchesSwitch);
+                }}
+              />
+
+              <ToggleSwitch
+                onColor="rgb(253,161,144)"
+                offColor="rgb(83,165,196)"
+                label="Request"
+                labelStyle={{
+                  color: "rgb(83,165,196)",
+                  fontWeight: "500",
+                  fontSize: 20
+                }}
+                size="large"
+                isOn={this.state.isOnRequestSwitch}
+                onToggle={isOnRequestSwitch => {
+                  this.setState({ isOnRequestSwitch });
+                  this.onToggle(isOnRequestSwitch);
+                }}
+              />
+              <ToggleSwitch
+                onColor="rgb(253,161,144)"
+                offColor="rgb(83,165,196)"
+                label="Messages"
+                labelStyle={{
+                  color: "rgb(83,165,196)",
+                  fontWeight: "500",
+                  fontSize: 20
+                }}
+                size="large"
+                isOn={this.state.isOnMessagesSwitch}
+                onToggle={isOnMessagesSwitch => {
+                  this.setState({ isOnMessagesSwitch });
+                  this.onToggle(isOnMessagesSwitch);
+                }}
+              />
+            </View>
+          </View>
+        </View>
       </LinearGradient>
     );
   }
@@ -26,12 +160,31 @@ export default class Settings extends React.Component<SettingsProps> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgb(232,238,241)",
+    padding: 10
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  header: {
+    color: "rgb(83,165,196)",
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 10
   },
+
+  agerow: {
+    backgroundColor: "rgb(232,238,241)",
+    padding: 10
+  },
+
+  showmerow: {
+    flex: 1,
+    backgroundColor: "rgb(232,238,241)",
+    padding: 10
+  },
+
+  messagerow: {
+    flex: 1,
+    backgroundColor: "rgb(232,238,241)",
+    padding: 10
+  }
 });
