588ac728bd6b99b3ef41b6b86f27e1747738b42f
Sonya Wong <sonyawongpro@gmail.com>
2018-08-31 17:27:03 +0800

    updated control room page

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -1,131 +1,150 @@
-import * as React from 'react';
-import { Navigation } from 'react-native-navigation';
-import {transparentNav} from './screens/styles/common';
+import * as React from "react";
+import { Navigation } from "react-native-navigation";
+import { transparentNav } from "./screens/styles/common";
 
-import Landing from './screens/Landing';
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import SignupCont from './screens/SignupCont';
-import MbtiTest from './screens/MbtiTest';
-import MbtiResult from './screens/MbtiResult';
-import MbtiProfile from './screens/MbtiProfile';
-import Deck from './screens/Deck'; // home root
-import Chat from './screens/Chat';
-import Request from './screens/Request';
-import User from './screens/User';
-import Settings from './screens/Settings';
-import Profile from './screens/Profile';
+import Landing from "./screens/Landing";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import SignupCont from "./screens/SignupCont";
+import MbtiTest from "./screens/MbtiTest";
+import MbtiResult from "./screens/MbtiResult";
+import MbtiProfile from "./screens/MbtiProfile";
+import Deck from "./screens/Deck"; // home root
+import Chat from "./screens/Chat";
+import Request from "./screens/Request";
+import User from "./screens/User";
+import Settings from "./screens/Settings";
+import Profile from "./screens/Profile";
+import ResetProfile from "./screens/ResetProfile";
 
-import {store} from './store/store';
-import {Provider} from 'react-redux';
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 // import {connect} from 'react-redux';
-import {checkToken} from './actions/authAction';
-import {AsyncStorage} from 'react-native'
+import { checkToken } from "./actions/authAction";
+import { AsyncStorage } from "react-native";
 
-Navigation.registerComponent('LandingScreen', () => Landing, store, Provider); 
-Navigation.registerComponent('LoginScreen', () => Login, store, Provider); 
-Navigation.registerComponent('SignupScreen', () => Signup, store, Provider);
-Navigation.registerComponent('SignupContScreen', () => SignupCont, store, Provider);
-Navigation.registerComponent('MbtiTestScreen', () => MbtiTest, store, Provider);
-Navigation.registerComponent('MbtiResultScreen', () => MbtiResult, store, Provider);
-Navigation.registerComponent('MbtiProfileScreen', () => MbtiProfile, store, Provider);
-Navigation.registerComponent('HomeTabScreen', () => Deck, store, Provider);
-Navigation.registerComponent('ChatTabScreen', () => Chat, store, Provider);
-Navigation.registerComponent('ReqestTabScreen', () => Request, store, Provider);
-Navigation.registerComponent('UserTabScreen', () => User, store, Provider);
-Navigation.registerComponent('Settings', () => Settings, store, Provider); 
-Navigation.registerComponent('ProfileScreen', () => Profile, store, Provider);
-
-
+Navigation.registerComponent("LandingScreen", () => Landing, store, Provider);
+Navigation.registerComponent("LoginScreen", () => Login, store, Provider);
+Navigation.registerComponent("SignupScreen", () => Signup, store, Provider);
+Navigation.registerComponent(
+  "SignupContScreen",
+  () => SignupCont,
+  store,
+  Provider
+);
+Navigation.registerComponent("MbtiTestScreen", () => MbtiTest, store, Provider);
+Navigation.registerComponent(
+  "MbtiResultScreen",
+  () => MbtiResult,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiProfileScreen",
+  () => MbtiProfile,
+  store,
+  Provider
+);
+Navigation.registerComponent("HomeTabScreen", () => Deck, store, Provider);
+Navigation.registerComponent("ChatTabScreen", () => Chat, store, Provider);
+Navigation.registerComponent("ReqestTabScreen", () => Request, store, Provider);
+Navigation.registerComponent("UserTabScreen", () => User, store, Provider);
+Navigation.registerComponent("Settings", () => Settings, store, Provider);
+Navigation.registerComponent("ProfileScreen", () => Profile, store, Provider);
+Navigation.registerComponent(
+  "ResetProfile",
+  () => ResetProfile,
+  store,
+  Provider
+);
 
 Navigation.startSingleScreenApp({
-    screen: {
-        screen: 'LandingScreen',
-        navigatorStyle: {
-            navBarHidden: true, 
-        }
+  screen: {
+    screen: "LoginScreen",
+    navigatorStyle: {
+      navBarHidden: true
     }
-})
+  }
+});
 
 interface AppProps {
-    navigator: Navigator,
-    initialApp: () => void,
-    loginApp: () => void,
+  navigator: Navigator;
+  initialApp: () => void;
+  loginApp: () => void;
 }
 
 class App extends React.Component<AppProps> {
-    constructor(props: AppProps) {
-        super(props);
-        
-        // check login status when re-opening the app
+  constructor(props: AppProps) {
+    super(props);
+
+    // check login status when re-opening the app
     //     store.dispatch(checkToken()).then(loggedIn => {
-	// 		if (loggedIn) {
-	// 			App.loginApp();
-	// 		} else {
-	// 			App.initialApp();
-	// 		}
+    // 		if (loggedIn) {
+    // 			App.loginApp();
+    // 		} else {
+    // 			App.initialApp();
+    // 		}
     // 	});
-    
-    }
+  }
 
-    static initialApp() {
-        Navigation.startSingleScreenApp({
-            screen: {
-                screen: 'SignupContScreen',
-                navigatorStyle: {
-                    navBarHidden: true, 
-                }
-            }
-        })
-    }
+  static initialApp() {
+    Navigation.startSingleScreenApp({
+      screen: {
+        screen: "SignupContScreen",
+        navigatorStyle: {
+          navBarHidden: true
+        }
+      }
+    });
+  }
 
-    static loginApp() {
-        Navigation.startTabBasedApp({
-            tabs: [
-            {
-                label: 'Request',
-                screen: 'ReqestTabScreen', 
-                icon: require('./assets/deck.png'),
-                title: 'Request',
-                navigatorStyle: {transparentNav}
-            },{
-                label: 'Lucky Draw',
-                screen: 'ReqestTabScreen',
-                icon: require('./assets/deck.png'),
-                title: 'Lucky Draw',
-                navigatorStyle: {transparentNav}
-            },
-            {
-                label: 'Home Deck',
-                screen: 'HomeTabScreen',
-                icon: require('./assets/home.png'),
-                title: 'My Deck',
-                navigatorStyle: {transparentNav}
-            },
-            {
-                label: 'Notifications',
-                screen: 'ChatTabScreen',
-                icon: require('./assets/chat.png'),
-                title: 'Chat',
-                navigatorStyle: {transparentNav}
-            },
-            {
-                label: 'Control Room',
-                screen: 'UserTabScreen',
-                icon: require('./assets/user.png'),
-                title: 'Profile',
-                navigatorStyle: {transparentNav}
-            }
-            ],
-            tabsStyle: { 
-            initialTabIndex: 2, // optional, the default selected bottom tab. Default: 0. On Android, add this to appStyle
-            // tabBarButtonColor: '#ffff00', // optional, change the color of the tab icons and text (also unselected). On Android, add this to appStyle
-            tabBarSelectedButtonColor: '#F0957F', // optional, change the color of the selected tab icon and text (only selected). On Android, add this to appStyle
-            // tabBarBackgroundColor: '#551A8B', // optional, change the background color of the tab bar
-            },
-        })
-    }
-
+  static loginApp() {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          label: "Request",
+          screen: "ReqestTabScreen",
+          icon: require("./assets/deck.png"),
+          title: "Request",
+          navigatorStyle: { transparentNav }
+        },
+        {
+          label: "Lucky Draw",
+          screen: "ReqestTabScreen",
+          icon: require("./assets/deck.png"),
+          title: "Lucky Draw",
+          navigatorStyle: { transparentNav }
+        },
+        {
+          label: "Home Deck",
+          screen: "HomeTabScreen",
+          icon: require("./assets/home.png"),
+          title: "My Deck",
+          navigatorStyle: { transparentNav }
+        },
+        {
+          label: "Notifications",
+          screen: "ChatTabScreen",
+          icon: require("./assets/chat.png"),
+          title: "Chat",
+          navigatorStyle: { transparentNav }
+        },
+        {
+          label: "Control Room",
+          screen: "UserTabScreen",
+          icon: require("./assets/user.png"),
+          title: "Profile",
+          navigatorStyle: { transparentNav }
+        }
+      ],
+      tabsStyle: {
+        initialTabIndex: 2, // optional, the default selected bottom tab. Default: 0. On Android, add this to appStyle
+        // tabBarButtonColor: '#ffff00', // optional, change the color of the tab icons and text (also unselected). On Android, add this to appStyle
+        tabBarSelectedButtonColor: "#F0957F" // optional, change the color of the selected tab icon and text (only selected). On Android, add this to appStyle
+        // tabBarBackgroundColor: '#551A8B', // optional, change the background color of the tab bar
+      }
+    });
+  }
 }
 
 export default App;
@@ -135,10 +154,10 @@ export default App;
 //       isLoggedIn: state.authReducer
 //     }
 //   }
-  
+
 //   const mapDispatchToProps = (dispatch: any) => ({
 //     checkAuth: () => dispatch(checkAuth())
 //   })
-  
+
 //   const App = connect( mapDispatchToProps)(PureApp);
 //   export default App;
--- frontend/src/screens/User.tsx
+++ frontend/src/screens/User.tsx
@@ -1,50 +1,60 @@
-import * as React from 'react';
-import LinearGradient from 'react-native-linear-gradient';
+import * as React from "react";
+import LinearGradient from "react-native-linear-gradient";
 import {
-    Platform, 
-    StyleSheet, 
-    Text, 
-    View,
-    TouchableOpacity
-} from 'react-native';
-import {connect} from 'react-redux';
-import {logoutUser} from '../actions/authAction';
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authAction";
 
 interface UserProps {
-    navigator: Navigator,
-    onLogoutPress: () => void
-
+  navigator: Navigator;
+  onLogoutPress: () => void;
 }
 
 class PureUser extends React.Component<UserProps> {
-
   static navigatorButtons = {
-    rightButtons: [     
+    rightButtons: [
       {
-        icon: require('../assets/settings.png'), // for icon button, provide the local image asset name
-        id: 'settings' // id for this button, given in onNavigatorEvent(event) to help understand which button was clicked
+        icon: require("../assets/settings.png"), // for icon button, provide the local image asset name
+        id: "settings" // id for this button, given in onNavigatorEvent(event) to help understand which button was clicked
       }
     ]
   };
 
   render() {
     return (
-      <LinearGradient colors={['#9EF8E4', '#30519B']} style={[{flex: 1}]}>
-      <View style={styles.container}>
-          <TouchableOpacity onPress={() => this.props.navigator.push({
-              screen: 'Settings',
-              title: 'Settings'
-          })}>
-              <Text style={styles.welcome}>Click to see Setting</Text>
+      <LinearGradient colors={["#9EF8E4", "#30519B"]} style={[{ flex: 1 }]}>
+        <View style={styles.container}>
+          <TouchableOpacity
+            onPress={() =>
+              this.props.navigator.push({
+                screen: "Settings",
+                title: "Settings"
+              })
+            }
+          >
+            <Text style={styles.welcome}>Setting</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => 
-              this.props.onLogoutPress()
-            }>
-              <Text style={styles.welcome}>Logout</Text>
+          <TouchableOpacity
+            onPress={() =>
+              this.props.navigator.push({
+                screen: "ResetProfile",
+                title: "Edit Profile"
+              })
+            }
+          >
+            <Text style={styles.welcome}>Edit Profile</Text>
           </TouchableOpacity>
 
-      </View>
+          <TouchableOpacity onPress={() => this.props.onLogoutPress()}>
+            <Text style={styles.welcome}>Logout</Text>
+          </TouchableOpacity>
+        </View>
       </LinearGradient>
     );
   }
@@ -53,25 +63,28 @@ class PureUser extends React.Component<UserProps> {
 const MapStateToProps = (state: any) => {
   return {
     isLoggedIn: state.authReducer
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => ({
   onLogoutPress: () => dispatch(logoutUser())
-})
+});
 
-const User = connect(MapStateToProps, mapDispatchToProps)(PureUser);
+const User = connect(
+  MapStateToProps,
+  mapDispatchToProps
+)(PureUser);
 export default User;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    textAlign: "center",
+    margin: 10
+  }
 });
