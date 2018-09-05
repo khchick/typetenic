a7563226a9b3de6a28bba6e8654d7b3fda78a395
Sonya Wong <sonyawongpro@gmail.com>
2018-09-05 17:49:35 +0800

    added suggested page

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {AsyncStorage} from 'react-native'
+import { AsyncStorage } from "react-native";
 import { Navigation } from "react-native-navigation";
 import { transparentNav } from "./screens/styles/common";
 
@@ -9,14 +9,14 @@ import Signup from "./screens/Signup";
 import SignupCont from "./screens/SignupCont";
 // with redux
 import MbtiTest1 from "./screens/MbtiTest1";
-import MbtiTest2 from './screens/MbtiTest2';
-import MbtiTest3 from './screens/MbtiTest3';
-import MbtiTest4 from './screens/MbtiTest4';
+import MbtiTest2 from "./screens/MbtiTest2";
+import MbtiTest3 from "./screens/MbtiTest3";
+import MbtiTest4 from "./screens/MbtiTest4";
 // previous
 import MbtiTestQ1 from "./screens/MbtiTestQ1";
-import MbtiTestQ2 from './screens/MbtiTestQ2';
-import MbtiTestQ3 from './screens/MbtiTestQ3';
-import MbtiTestQ4 from './screens/MbtiTestQ4';
+import MbtiTestQ2 from "./screens/MbtiTestQ2";
+import MbtiTestQ3 from "./screens/MbtiTestQ3";
+import MbtiTestQ4 from "./screens/MbtiTestQ4";
 
 import MbtiResult from "./screens/MbtiResult";
 import MbtiInfoLightBox from "./screens/MbtiInfoLightBox";
@@ -28,51 +28,124 @@ import User from "./screens/User";
 import Settings from "./screens/Settings";
 import Profile from "./screens/Profile";
 import ResetProfile from "./screens/ResetProfile";
+import Suggested from "./screens/Suggested";
 
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { loginSuccess } from "./redux/actions/authAction";
 
-
 Navigation.registerComponent("LandingScreen", () => Landing, store, Provider);
 Navigation.registerComponent("LoginScreen", () => Login, store, Provider);
 Navigation.registerComponent("SignupScreen", () => Signup, store, Provider);
-Navigation.registerComponent("SignupContScreen", () => SignupCont,store,Provider);
-Navigation.registerComponent("MbtiTest1Screen", () => MbtiTest1, store, Provider);
-Navigation.registerComponent('MbtiTest2Screen', () => MbtiTest2, store, Provider);
-Navigation.registerComponent('MbtiTest3Screen', () => MbtiTest3, store, Provider);
-Navigation.registerComponent('MbtiTest4Screen', () => MbtiTest4, store, Provider);
-Navigation.registerComponent("MbtiResultScreen", () => MbtiResult, store, Provider);
-Navigation.registerComponent("MbtiInfoLightBox", () => MbtiInfoLightBox, store, Provider);
-Navigation.registerComponent("MbtiProfileScreen", () => MbtiProfile, store, Provider);
+Navigation.registerComponent(
+  "SignupContScreen",
+  () => SignupCont,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTest1Screen",
+  () => MbtiTest1,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTest2Screen",
+  () => MbtiTest2,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTest3Screen",
+  () => MbtiTest3,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTest4Screen",
+  () => MbtiTest4,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiResultScreen",
+  () => MbtiResult,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiInfoLightBox",
+  () => MbtiInfoLightBox,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiProfileScreen",
+  () => MbtiProfile,
+  store,
+  Provider
+);
 Navigation.registerComponent("HomeTabScreen", () => Deck, store, Provider);
 Navigation.registerComponent("ChatTabScreen", () => Chat, store, Provider);
-Navigation.registerComponent("RequestTabScreen", () => Request, store, Provider);
+Navigation.registerComponent(
+  "RequestTabScreen",
+  () => Request,
+  store,
+  Provider
+);
 Navigation.registerComponent("UserTabScreen", () => User, store, Provider);
 Navigation.registerComponent("Settings", () => Settings, store, Provider);
+Navigation.registerComponent(
+  "SuggestedScreen",
+  () => Suggested,
+  store,
+  Provider
+);
 Navigation.registerComponent("ProfileScreen", () => Profile, store, Provider);
-Navigation.registerComponent("ResetProfile", () => ResetProfile, store, Provider);
-
-Navigation.registerComponent("MbtiTestQ1Screen", () => MbtiTestQ1, store, Provider);
-Navigation.registerComponent('MbtiTestQ2Screen', () => MbtiTestQ2, store, Provider);
-Navigation.registerComponent('MbtiTestQ3Screen', () => MbtiTestQ3, store, Provider);
-Navigation.registerComponent('MbtiTestQ4Screen', () => MbtiTestQ4, store, Provider);
-
-
+Navigation.registerComponent(
+  "ResetProfile",
+  () => ResetProfile,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTestQ1Screen",
+  () => MbtiTestQ1,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTestQ2Screen",
+  () => MbtiTestQ2,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTestQ3Screen",
+  () => MbtiTestQ3,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "MbtiTestQ4Screen",
+  () => MbtiTestQ4,
+  store,
+  Provider
+);
 
 class App {
   constructor() {
     // check login status when re-opening the app
-    AsyncStorage.getItem('token').then((token: string|null) => {
+    AsyncStorage.getItem("token").then((token: string | null) => {
       if (token) {
         App.loginApp(token);
-        store.dispatch(loginSuccess(token))
+        store.dispatch(loginSuccess(token));
       } else {
         App.initialApp();
       }
     });
   }
-  
+
   static fbAppendProfile() {
     Navigation.startSingleScreenApp({
       screen: {
@@ -99,17 +172,17 @@ class App {
     Navigation.startTabBasedApp({
       tabs: [
         {
+          label: "Suggest",
+          screen: "SuggestedScreen",
+          icon: require("./assets/deck.png"),
+          title: "Suggest",
+          navigatorStyle: { transparentNav }
+        },
+        {
           label: "Request",
           screen: "RequestTabScreen",
           icon: require("./assets/deck.png"),
           title: "Request",
-          navigatorStyle: { transparentNav }
-        },
-        {
-          label: "Lucky Draw",
-          screen: "RequestTabScreen",
-          icon: require("./assets/deck.png"),
-          title: "Lucky Draw",
           navigatorStyle: { transparentNav }
         },
         {
@@ -130,7 +203,7 @@ class App {
         //   navigatorStyle: { transparentNav }
         // },
         {
-          label: "Control Room",
+          label: "Profile",
           screen: "UserTabScreen",
           icon: require("./assets/user.png"),
           title: "Profile",
