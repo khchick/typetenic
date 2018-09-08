2811caefeda3485b8cd75ed570ae0c661d7eb444
kinghei.chick <khchick@gmail.com>
2018-09-09 02:04:21 +0800

    Created table for notifications

---
+++ backend/migrations/20180908202519_notification-table.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex){
+    return knex.schema.createTable('notification',(table)=>{
+        table.increments('id').primary();
+        table.string('title');
+        table.text('content');
+        table.enu('read_status', ['Unread', 'Read']);
+        table.enu('type', ['Request Received', 'Request Approved', 'Request Rejected', 'Flip Request Received', 'Flip Request Approved', 'Flip Request Rejected', 'Connection Lost'])
+        table.integer('type_id');
+        table.integer('note_receiver_id').unsigned();
+        table.foreign('note_receiver_id').references('users.id');
+        table.timestamps(false, true);
+    });
+  }
+  
+  exports.down = function(knex){
+    return knex.schema.dropTable('notification');
+  }
