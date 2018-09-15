f3b861ffbef899f390e7fcdec21aa0f5b1919786
Wesley Yung <ysywesley@gmail.com>
2018-09-15 23:52:48 +0800

    .

--- frontend/src/screens/styles/common.js
+++ frontend/src/screens/styles/common.js
@@ -1,7 +1,14 @@
-import { StyleSheet } from 'react-native'; 
-import {Dimensions} from 'react-native';
- 
-export const {height, width} = Dimensions.get('window');
+import {
+    StyleSheet
+} from 'react-native';
+import {
+    Dimensions
+} from 'react-native';
+
+export const {
+    height,
+    width
+} = Dimensions.get('window');
 
 
 // export const navigatorStyle = {
@@ -23,21 +30,24 @@ export const globalStyle = StyleSheet.create({
         padding: 10,
         fontFamily: 'Futura',
         letterSpacing: 2,
-      },
+    },
     // building MBTI profile
     cardContainer: {
         alignItems: 'center',
         backgroundColor: 'white',
         padding: 30,
-
         borderRadius: 15,
         shadowColor: '#000',
-        shadowOffset: { width: 2, height: 2 },
+        shadowOffset: {
+            width: 2,
+            height: -3
+        },
         shadowOpacity: 0.4,
-        shadowRadius: 6,
+        shadowRadius: 2,
         width: width * 0.85,
         // height: height * 0.85,
-        marginTop: 40,
+        marginTop: 15,
+        marginBottom: 15
     },
     title: {
         fontSize: 20,
@@ -55,14 +65,14 @@ export const globalStyle = StyleSheet.create({
     },
 
 });
-  
+
 
 export const answer = {
     color: '#30519B',
     marginTop: 15,
     marginBottom: 5,
 }
-  
+
 export const chosenAnswer = {
     fontWeight: '500',
     color: '#ed755a',
